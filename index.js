3a6decde497984810fdeb271f353cc725ecd59e5
pabloribera <pabloriberanacher@gmail.com>
2024-09-24 19:35:02 +0200

    Comentarios actualizados

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const port = 3001
 
+//Cuando llegue a la rama raiz que muestre por pantalla el siguiente mensaje.
 app.get("/",function(req,res){
     res.send("HOLA")
 })
